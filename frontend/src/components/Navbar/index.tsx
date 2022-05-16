e1145c61438c82a3a03bb26cf02d91621dedf586
Beatriz Mayorka <mayorka_beatriz@hotmail.com.br>
2022-05-15 23:49:56 -0300

    Some settings

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -2,12 +2,13 @@ import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
 import './styles.css';
 
 function Navbar() {
+
     return (
         <header>
             <nav className="container">
                 <div className="dsmovie-nav-content">
                     <h1>DSMovie</h1>
-                    <a href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
+                    <a href="https://github.com/devsuperior">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
                             <p className="dsmovie-contact-link">/devsuperior</p>
